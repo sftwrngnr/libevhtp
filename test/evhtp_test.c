4431cf6a651f39701dae11afe15dec4014b0d937
Dan Henderson <sftwrngnr66@gmail.com>
2017-10-21 16:40:39 -0700

    Added simple (null) test case as a placeholder. Test builds as expected. Directory structure will work for now. Now onto "real" work, the business of actual, meaningful test cases.

---
+++ test/evhtp_test.c
@@ -0,0 +1,16 @@
+/***************************************************/
+/* evhtp_test.c                                                        */
+/* Unit test for evhtp.c                                            */
+/*                                                                            */
+/* Written by Dan Henderson                                  */
+/* Copyright (C) Critical-stack 2017                        */
+/***************************************************/
+
+#include <test_evhtp.h>
+#include <evhtp/evhtp.h>
+
+int main(int argc, char ** argp)
+{
+    return 0; /* Dummy fail for now */
+}
+
---
+++ test/include/test_evhtp.h
@@ -0,0 +1,7 @@
+#ifndef test_evhtp_h
+#define test_evhtp_h
+#include <stdlib.h>
+
+void * htp__malloc_(size_t size);
+
+#endif
