04d84c7277cfbe205d30d2ca1bfeeaf295ca0990
Dan Henderson <sftwrngnr66@gmail.com>
2017-10-20 18:04:28 -0700

    Adding unit tests. Going to have to get rid of a bunch of extraneous crap related to cmake.

---
+++ test/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhenders/work/libevhtp/tests
+# Build directory: /home/dhenders/work/libevhtp/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(ehtp_test "WORKING_DIRECTORY" "tests" "ehtp_test")
+add_test(numtoa_test "WORKING_DIRECTORY" "tests" "numtoa_test")
+add_test(parser_test "WORKING_DIRECTORY" "tests" "parser_test")
+add_test(thread_test "WORKING_DIRECTORY" "tests" "thread_test")
---
+++ test/evhtp_test.c
@@ -0,0 +1,11 @@
+/***************************************************/
+/* evhtp_test.c                                                        */
+/* Unit test for evhtp.c                                            */
+/*                                                                            */
+/* Written by Dan Henderson                                  */
+/* Copyright (C) Critical-stack 2017                        */
+/***************************************************/
+
+#include "evhtp.h"
+void * htp__malloc_(size_t size);
+
